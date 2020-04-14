055e1022f3225d3f1073964ac3b574dde5f66110
adrianclo <adrianclo1984@gmail.com>
2020-04-14 15:38:12 +0200

    sensitive neighbour expansion

--- corona_workout.R
+++ corona_workout.R
@@ -1,114 +1,124 @@
 library(tidyverse)
+library(tibble)
 
 #' (1) full work out
-#' (2) planc
-#' (3) chair
+#' (2) planc variants
+#' (3) chair and squats
 set <- 1
 
 # extra settings
-jumping_rope <- T # replace jumping rope?
-jumping_jack <- T # replace jumping jack?
-burpees <- T      # replace burpees?
-extra_hard <- F   # >25 are counted backwards, and workout is finished once landing exactly on 25
-level <- c()      # easy - medium - hard
+sensitive_neighbour <- T # replace all jumping activities with quiet ones
+extra_hard <- F          # >25 are counted backwards, and workout is finished once landing exactly on 25
+level <- "easy"          # easy - medium - hard
 
 # click SOURCE to get today's corona workout printed out for you!
 
 game_list <- list(
-  
+  full_work_out = tibble::tribble(
+    ~square, ~instruction,     ~set, ~rep, ~unit, ~extra,
+    "1",  "planc",              "1", "1",  "min", NA,
+    "2",  "upward bridge",      "1", "10", "rep", NA,
+    "3",  "superman",           "1", "30", "s",   NA,
+    "4",  "lunges",             "1", "10", "rep", "per leg",
+    "5",  "jumping jacks",      "1", "30", "s",   NA,
+    "6",  "lateral squats",     "1", "10", "rep", "per side",
+    "7",  "REPEAT", NA,NA,NA,NA, 
+    "8",  "mountain climbers",  "1", "30", "s",   NA,
+    "9",  "sumo squats",        "1", "12", "rep", NA,
+    "10", "jumping ropes",      "1", "30", "s",   NA,
+    "11", "lateral planc",      "1", "30", "s",   "per side",
+    "12", "dips",               "1", "10", "rep", NA,
+    "13", "push ups",           "1", "8",  "rep", NA,
+    "14", "GO BACK 3 STEPS", NA,NA,NA,NA,
+    "15", "squats",             "1", "15", "rep", NA,
+    "16", "chair stand",        "1", "30", "s",   NA,
+    "17", "donkey kicks",       "1", "10", "rep", "per leg",
+    "18", "jumping squats",     "1", "8",  "rep", NA,
+    "19", "RESTART", NA,NA,NA,NA,
+    "20", "burpees",            "1", "8",  "rep", NA,
+    "21", "v planc",            "1", "30", "s",   NA,
+    "22", "squats",             "1", "20", "rep", NA,
+    "23", "GO BACK 2 STEPS", NA,NA,NA,NA,
+    "24", "stationary running", "1", "30", "s",   NA,
+    "25", "chair stand",        "1", "30", "s",   NA
+  ),
+  planc = tibble::tribble(),
+  chair = tibble::tribble()
 )
 
-game <- matrix(c(
-  # square instruction set rep unit extra
-  "1",  "planc",              "1", "1",  "min",  NA,
-  "2",  "upward bridge",      "1", "10", "rep", NA,
-  "3",  "superman",           "1", "30", "s",   NA,
-  "4",  "lunges",             "1", "10", "rep", "per leg",
-  "5",  "jumping jacks",      "1", "30", "s", NA,
-  "6",  "lateral squats",     "1", "10", "rep", "per side",
-  "7",  "REPEAT", NA,NA,NA,NA, 
-  "8",  "horizontal mountain climbers",  "1", "30", "s", NA,
-  "9",  "sumo squats",        "1", "12", "rep", NA,
-  "10", "jumping ropes",      "1", "30", "s", NA,
-  "11", "lateral planc",      "1", "30", "s", "per side",
-  "12", "dips",               "1", "10", "rep", NA,
-  "13", "push ups",           "1", "8",  "rep", NA,
-  "14", "GO BACK 3 STEPS", NA,NA,NA,NA,
-  "15", "squats",             "1", "15", "rep", NA,
-  "16", "chair stand",        "1", "30", "s", NA,
-  "17", "donkey kicks",       "1", "10", "rep", "per leg",
-  "18", "jumping squats",     "1", "8",  "rep", NA,
-  "19", "RESTART", NA,NA,NA,NA,
-  "20", "burpees",            "1", "8",  "rep", NA,
-  "21", "v planc",            "1", "30", "s", NA,
-  "22", "squats",             "1", "20", "rep", NA,
-  "23", "GO BACK 2 STEPS", NA,NA,NA,NA,
-  "24", "stationary running", "1", "30", "s", NA,
-  "25", "chair stand",        "1", "30", "s", NA
-  ), nrow = 25, byrow = T)
-colnames(game) <- c("square", "instruction", "set", "time", "unit", "extra")
-game <- as_tibble(game)
+game <- game_list[[set]]
 
-if(jumping_rope) game[10,2] <- "stationary running"
-if(jumping_jack) game[5,2] <- "standing mountain climbers"
-if(burpees) game[20,2] <- "push ups"
+if(sensitive_neighbour & set == 1) {
+  game[5,2] <- "superman"
+  game[8,c(2,4,5)] <- c("push ups", 20, "rep")
+  game[10,c(2,5)] <- c("lateral planc", "per side")
+  game[18,2] <- "push ups"
+  game[20,2] <- "push ups"
+  game[24,2] <- "planc"
+}
 # game
 
-game_now <- tibble()
-cumul_step <- 0
-while( cumul_step < 25 ) {
-  dice_roll <- sample(1:6, 1)
-  cumul_step <- sum(cumul_step, dice_roll)
-  
-  print(cumul_step)
-  if(!extra_hard) {
-    if(cumul_step == 7) {
-      game_now <- bind_rows(game_now, tail(game_now, 1))
-    } else if(cumul_step == 14) {
-      game_now <- bind_rows(game_now, slice(game, 11))
-      cumul_step <- 11
-    } else if(cumul_step == 19) {
-      game_now <- bind_rows(game_now, slice(game, 1))
-      cumul_step <- 1
-    } else if(cumul_step == 23) { 
-      game_now <- bind_rows(game_now, slice(game, 21))
-      cumul_step <- 21
-    } else if(cumul_step == 25) {
-      game_now <- bind_rows(game_now, slice(game, cumul_step))
-      cat("You are finished for today. Come back tomorrow!")
-      break()
-    }
-    else if(cumul_step > 25) {
-      cat("You are finished for today. Come back tomorrow!")
-      break()
+if(set == 1) {
+  game_now <- tibble()
+  cumul_step <- 0
+  while( cumul_step < 25 ) {
+    dice_roll <- sample(1:6, 1)
+    cumul_step <- sum(cumul_step, dice_roll)
+    
+    print(cumul_step)
+    if(!extra_hard) {
+      if(cumul_step == 7) {
+        game_now <- bind_rows(game_now, tail(game_now, 1))
+      } else if(cumul_step == 14) {
+        game_now <- bind_rows(game_now, slice(game, 11))
+        cumul_step <- 11
+      } else if(cumul_step == 19) {
+        game_now <- bind_rows(game_now, slice(game, 1))
+        cumul_step <- 1
+      } else if(cumul_step == 23) { 
+        game_now <- bind_rows(game_now, slice(game, 21))
+        cumul_step <- 21
+      } else if(cumul_step == 25) {
+        game_now <- bind_rows(game_now, slice(game, cumul_step))
+        cat("You are finished for today. Come back tomorrow!")
+        break()
+      }
+      else if(cumul_step > 25) {
+        cat("You are finished for today. Come back tomorrow!")
+        break()
+      } else {
+        game_now <- bind_rows(game_now, slice(game, cumul_step))
+      }
     } else {
-      game_now <- bind_rows(game_now, slice(game, cumul_step))
-    }
-  } else {
-    if(cumul_step == 7) {
-      game_now <- bind_rows(game_now, tail(game_now, 1))
-    } else if(cumul_step == 14) {
-      game_now <- bind_rows(game_now, slice(game, 11))
-      cumul_step <- 11
-    } else if(cumul_step == 19) {
-      game_now <- bind_rows(game_now, slice(game, 1))
-      cumul_step <- 1
-    } else if(cumul_step == 23) { 
-      game_now <- bind_rows(game_now, slice(game, 21))
-      cumul_step <- 21
-    } else if(cumul_step == 25) {
-      game_now <- bind_rows(game_now, slice(game, cumul_step))
-      cat("You are finished for today. Come back tomorrow!")
-      break()
-    }
-    else if(cumul_step > 25) {
-      cumul_step <- 25 - cumul_step + 25
-      game_now <- bind_rows(game_now, slice(game, cumul_step))
-    } else {
-      game_now <- bind_rows(game_now, slice(game, cumul_step))
+      if(cumul_step == 7) {
+        game_now <- bind_rows(game_now, tail(game_now, 1))
+      } else if(cumul_step == 14) {
+        game_now <- bind_rows(game_now, slice(game, 11))
+        cumul_step <- 11
+      } else if(cumul_step == 19) {
+        game_now <- bind_rows(game_now, slice(game, 1))
+        cumul_step <- 1
+      } else if(cumul_step == 23) { 
+        game_now <- bind_rows(game_now, slice(game, 21))
+        cumul_step <- 21
+      } else if(cumul_step == 25) {
+        game_now <- bind_rows(game_now, slice(game, cumul_step))
+        cat("You are finished for today. Come back tomorrow!")
+        break()
+      }
+      else if(cumul_step > 25) {
+        cumul_step <- 25 - cumul_step + 25
+        game_now <- bind_rows(game_now, slice(game, cumul_step))
+      } else {
+        game_now <- bind_rows(game_now, slice(game, cumul_step))
+      }
     }
   }
-}
+} else if(set == 2) {
+  
+} else if(set == 3) {
+  
+} else { stop("Your exercise set does not exist..") }
 
 game_now <- game_now %>% select(-square)
 
